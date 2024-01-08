3bc8c8b966209fd1d323a50e843769785ca89fc8
Wolf68k <Wolf68k@users.noreply.github.com>
2024-01-08 17:38:24 -0600

    Create trucksimulatorwiki.user.js

---
+++ trucksimulatorwiki.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Redirect to New Truck Simulator Wiki
+// @namespace
+// @version      1.0
+// @description  Redirect automatically from the old Truck Simulator Wiki (Fandom) to the new Wiki
+// @author       Wolf68k
+// @match        https://truck-simulator.fandom.com/wiki/*
+// @icon         https://trucksimulator.wiki.gg/favicon.ico
+// @run-at       document-start
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function() {
+    // only move the english wiki
+    if((location.host + location.pathname).indexOf("truck-simulator.fandom.com/wiki") == 0) {
+        location.replace(location.protocol + "//trucksimulator.wiki.gg" + location.pathname + location.search + location.hash);
+    }
+})();
